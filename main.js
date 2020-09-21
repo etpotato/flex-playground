fe857241e6e62b1a65513e758f2ad14663bef2cc
etpotatoo <slyboot93@mail.ru>
2020-09-21 17:51:48 +0300

    Правки функционала

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
 const container = document.querySelector('.flex-container');
-const buttonLr = document.querySelector('.button-lr');
-const buttonRl = document.querySelector('.button-rl');
+const buttonRow = document.querySelector('.button-row');
+const buttonColumn = document.querySelector('.button-column');
 
-buttonLr.addEventListener('click', function() {
-  if (container.classList.contains('mode-rl')) {
-    container.classList.remove('mode-rl');
-    container.classList.add('mode-lr');
-  } else {
-    container.classList.add('mode-lr');
-  }
-  buttonLr.blur();
+buttonRow.addEventListener('click', function() {
+  container.classList.remove('mode-column');
+  container.classList.add('mode-row');
+  buttonRow.blur();
 });
 
-buttonRl.addEventListener('click', function() {
-  if (container.classList.contains('mode-lr')) {
-    container.classList.remove('mode-lr');
-    container.classList.add('mode-rl');
-  } else {
-    container.classList.add('mode-rl');
-  }
-  buttonRl.blur();
+buttonColumn.addEventListener('click', function() {
+  container.classList.remove('mode-row');
+  container.classList.add('mode-column');
+  buttonColumn.blur();
 });
