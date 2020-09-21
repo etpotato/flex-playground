79e4ca88f7cbc566507f1f7a26be7a27abc8afa3
etpotatoo <slyboot93@mail.ru>
2020-09-21 22:49:51 +0300

    Правка работы кнопок

--- main.js
+++ main.js
@@ -5,11 +5,16 @@ const buttonColumn = document.querySelector('.button-column');
 buttonRow.addEventListener('click', function() {
   container.classList.remove('mode-column');
   container.classList.add('mode-row');
+  buttonRow.disabled = true;
+  buttonColumn.disabled = false;
   buttonRow.blur();
 });
 
 buttonColumn.addEventListener('click', function() {
   container.classList.remove('mode-row');
   container.classList.add('mode-column');
+  buttonRow.classList.remove('button-on');
+  buttonColumn.disabled = true;
+  buttonRow.disabled = false;
   buttonColumn.blur();
 });
